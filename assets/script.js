68b2e1a5eb9363c1273886ebb02f3ca5e0bbd5a9
Rob-Watson-84 <106341062+Rob-Watson-84@users.noreply.github.com>
2022-07-28 18:26:32 -0700

    added js variables, function for the search input, and comments of future functions to add.

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+//global variables
+var openWeatherID = 'GET API KEY ID FROM SITE';
+
+//html variables for searching
+var citySearch = $('#citySearch');
+var cityInput = $('#cityInput');
+var previousSearches = $('#previousSearches');
+var cityContainer = $('cityContainer');
+
+//search for the city based on user's search input
+function searchInput(event) {
+    event.preventDefault()
+    weatherData(cityInput.val())
+}
+
+//fetch api function for city name
+
+
+
+//fetch api based on lat & Long results from city name search
+
+
+
+
+//current weather search (city, date, time, icon, temp, humidity, wind, uv)
+//Change color for un index alerts
+
+
+
+//future forecast card section (5 days, date, icon, temp, humidity, wind, uv)
+
+
+
+//save to local storage
+
+
+
+//retrieve from local storage
+
+
+//init()
