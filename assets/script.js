0ba5876340d9449cd0ada2208f73b68fad16e33a
Rob-Watson-84 <106341062+Rob-Watson-84@users.noreply.github.com>
2022-07-30 16:47:08 -0700

    added fetch command to js and fixed variables to .querySelectors

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,34 @@
 //global variables
-var openWeatherID = 'GET API KEY ID FROM SITE';
+var openWeatherID = '72bfe64c1cb155d61ffbf6dcd2577a1d';
 
 //html variables for searching
-var citySearch = $('#citySearch');
-var cityInput = $('#cityInput');
-var previousSearches = $('#previousSearches');
-var cityContainer = $('cityContainer');
+var citySearch = document.querySelector('#citySearch');
+var cityInput = document.querySelector('#cityInput');
+var previousSearches = document.querySelector('#previousSearches');
+var cityContainer = document.querySelector('cityContainer');
 
 //search for the city based on user's search input
 function searchInput(event) {
     event.preventDefault()
-    weatherData(cityInput.val())
+    currentWeatherAPI(cityInput.val())
 }
 
 //fetch api function for city name
+function currentWeatherAPI(cityName) {
+    var url = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${cityName}&appid=${openWeatherId}';
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        cityName = data.name;
+        callUVIndex(cityName, data.coord.lon, data.coord.lat);
+        previousSearches(cityName, false);
+        })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 
+    return;
+}
 
 
 //fetch api based on lat & Long results from city name search
